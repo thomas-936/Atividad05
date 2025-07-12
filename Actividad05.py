90a690958e55e89867956bf35750a0a4019c4bfd
tomas <tomas27@gmail.com>
2025-07-12 00:02:43 -0600

    tercer commit

--- Actividad05.py
+++ Actividad05.py
@@ -18,3 +18,38 @@ def resgistrar_estudiante():
     nombre = input("Nombre: ")
     carnet = input("Carnet: ")
     carrera = input("Carrera: ")
+    nota_final = float(input("Ingrese la nota final: "))
+    estudiantes1 = estudiante(nombre, carnet, carrera, nota_final)
+    lista_estudiantes.append(estudiantes1)
+    print("Estudiante ingresado correctamente...")
+
+
+def mostrar_estidiantes():
+    print("Los estudiantes registrados son: ")
+    if len(lista_estudiantes)== 0:
+        print("\nNo hay estudiantes registrados... ")
+    else:
+        for estu in lista_estudiantes:
+            print(f"-{estu}")
+
+def buscar():
+    buscar_carnet = input("Ingrese el carnet ha buscar")
+    encontrado = 0
+    for i in lista_estudiantes:
+        if i.carnet == buscar_carnet:
+            print("Estudiante encontarado...")
+            encontrado=1
+            break
+
+    if encontrado ==0:
+        print("El estudiante no ha sido encontrado.")
+
+def calcular_promedio():
+    if len(lista_estudiantes) == 0:
+        print("No hay estudiantes registrados.")
+    else:
+        suma_total = 0
+        for j in lista_estudiantes:
+            suma_total+=j.nota_final
+        promedio = suma_total/ len(lista_estudiantes)
+        print(f"El promedio de notas de los estudiantes es: {promedio} ")
