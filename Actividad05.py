47769f39a454853565f245d52d58c01adcdca370
tomas <tomas27@gmail.com>
2025-07-11 23:18:26 -0600

    subir

--- Actividad05.py
+++ Actividad05.py
@@ -1,1 +1,20 @@
-print("actiiad 05")
+class estudiante:
+    def __init__(self, nombre, carnet, carrera, nota_final):
+        self.nombre = nombre
+        self.carnet = carnet
+        self.carrera = carrera
+        self.nota_final = nota_final
+
+        def mostar_info():
+            print(f"Nombre: {self.nombre}")
+            print(f"Carnet: {self.carnet}")
+            print(f"Carrera: {self.carrera}")
+            print(f"Nota final: {self.nota_final}")
+
+lista_estudiantes = []
+
+def resgistrar_estudiante():
+    print("Registrar nuevo estudiante: ")
+    nombre = input("Nombre: ")
+    carnet = input("Carnet: ")
+    carrera = input("Carrera: ")
